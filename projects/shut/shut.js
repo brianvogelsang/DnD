06bf1d560385378c8f24f0358b3f56ac4da70336
brianvogelsang <34783078+brianvogelsang@users.noreply.github.com>
2019-10-23 21:39:41 -0500

    Shut the Box v2
    Overhual of Shut-the-Box game

--- projects/shut/shut.js
+++ projects/shut/shut.js
@@ -1,9 +1,19 @@
 var yourRoll = 0;
+var boxes = [1,2,3,4,5,6,7,8,9];
+var score = 0;
 
 function reset(){
     yourRoll = 0;
+    boxes = [1,2,3,4,5,6,7,8,9];
     
-    document.getElementById('printOut').innerHTML = 'Total: ' + yourRoll;
+    document.getElementById('debug').innerHTML = '';
+    
+    document.getElementById('printOut').innerHTML = 'Roll the dice';
+    
+    document.getElementById('roll1Btn').style.display = 'none';
+    
+    document.getElementById('d1').style.display = 'none';
+    document.getElementById('d2').style.display = 'none';
     
     document.getElementById('b1').disabled = false;
     document.getElementById('b2').disabled = false;
@@ -16,35 +26,89 @@ function reset(){
     document.getElementById('b9').disabled = false;
 }
 
-function roll(){
-
-    var printout = document.getElementById('printOut');
+function roll2(){
     
-    var d1 = Math.floor(Math.random() * 6) + 1;
-    var d2 = Math.floor(Math.random() * 6) + 1;
+    if(yourRoll ==0){
+        score = boxes.reduce((a, b) => a + b, 0)
+        document.getElementById('debug').innerHTML = 'Score: -' + score;
     
+        var printout = document.getElementById('printOut');
     
-    if(yourRoll == 0){
-        yourRoll = d1 + d2;
-        document.getElementById('printOut').innerHTML = 'Total: ' + yourRoll;
+        var d1 = Math.floor(Math.random() * 6) + 1;
+        var d2 = Math.floor(Math.random() * 6) + 1;
     
+        document.getElementById('d1').innerHTML = d1;
+        document.getElementById('d2').innerHTML = d2;
+    
+        document.getElementById('d1').style.display = 'flex';
+        document.getElementById('d2').style.display = 'flex';
+    
+        if(yourRoll == 0){
+            yourRoll = d1 + d2;
+            document.getElementById('printOut').innerHTML = 'Dice Total: ' + yourRoll;
+    
+        }
     }
         
+}
+
+function roll1(){
+    if(yourRoll ==0){
+        score = boxes.reduce((a, b) => a + b, 0)
+        document.getElementById('debug').innerHTML = 'Score: -' + score;
+    
+    
+        var printout = document.getElementById('printOut');
+    
+        var d1 = Math.floor(Math.random() * 6) + 1;
+    
+        document.getElementById('d1').innerHTML = d1;
+    
+        document.getElementById('d1').style.display = 'flex';
+  
+    
+        if(yourRoll == 0){
+            yourRoll = d1;
+            document.getElementById('printOut').innerHTML = 'Dice Total: ' + yourRoll;
+    
+        }
+    }
 }
 
 function press(str){
     x = str[1];
     
-    //document.getElementById('debug').innerHTML = x;
+   
     
     if(yourRoll - x >= 0){
         yourRoll = yourRoll - x;
+        boxes[x-1] = 0;
         document.getElementById(str).disabled = true;
+        if(yourRoll == 0){
+            score = boxes.reduce((a, b) => a + b, 0)
+            document.getElementById('debug').innerHTML = 'Score: -' + score;
+            document.getElementById('printOut').innerHTML = 'Roll the dice';
+    
+            document.getElementById('d1').style.display = 'none';
+            document.getElementById('d2').style.display = 'none';
+            
+            if(score == 0){
+                document.getElementById('debug').innerHTML = 'YOU WIN';
+            }
+        }
     }
     
-    document.getElementById('printOut').innerHTML = 'Total: ' + yourRoll;
-   
+    if(yourRoll > 0){document.getElementById('printOut').innerHTML = 'Dice Total: ' + yourRoll;}
+    
+    if(boxes[6] + boxes[7] + boxes[8] == 0){
+        document.getElementById('roll1Btn').style.display = 'block';
+        
+        
+    
+    
+    }
 }
+   
 
 /*
 #! python3
